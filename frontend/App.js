73103b73203f1e7dd6b9e7ab0f0007bf02fdf2a3
Bill W <bill.bw82262@hotmail.com>
2022-07-09 14:30:18 +1200

    Added text box for  start/end sleep time

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SleepTime from './components/SleepTime';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to </Text>
       <StatusBar style="auto" />
+      <SleepTime />
     </View>
   );
 }
--- frontend/components/sleepTime.js
+++ frontend/components/sleepTime.js
@@ -1,17 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Form } from 'react-native';
+import React, { Component, useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to </Text>
-    </View>
-  );
+export default class SleepTime extends Component {
+
+    constructor(props){
+        super( props);
+        this.state = {
+            startTime: '',
+        }
+
+        this.handleStartTime = this.handleStartTime.bind(this);
+        this.handleEndTime = this.handleEndTime.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleStartTime (event) {
+        this.setState({
+            startTime: event.nativeEvent.text
+        })
+    } 
+
+    handleEndTime (event) {
+      this.setState({
+          endTime: event.nativeEvent.text
+      })
+  } 
+
+    handleSubmit = (event) => {
+        this.setState({
+
+        })
+    }
+
+
+  render() {
+        return  (
+        <View style={styles.container}>
+          <Text>Set Sleep Time</Text>
+          
+            <Text>Target Sleep Time</Text>
+            <TextInput
+                style={styles.input}
+                onChange={this.handleStartTime}
+                value={this.state.startTime}
+                placeholder="Start Time"
+            />
+
+            <Text>Target Wake Time</Text>
+            <TextInput
+                style={styles.input}
+                onChange={this.handleEndTime}
+                value={this.state.endTime}
+                placeholder="End Time"
+            />
+
+          
+          
+          
+        </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '',
     alignItems: 'center',
     justifyContent: 'center',
   },
