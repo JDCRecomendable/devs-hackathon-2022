fe29348165964b9ec5dd6e2656ec6f95383e9c49
Tony <tonycui02@gmail.com>
2022-07-09 14:22:49 +1200

    added cat gif

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Wow this s awd</Text> */}
       <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/BG.png')} resizeMode="cover" style={styles.image}>
+        <Image style={{ width: 300, height: 350 }} source={require('./assets/happy_cat_no_bg.gif')} />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +16,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
   },
 });
